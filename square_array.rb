515a7eb7fbba54110f47fef01a6dc5a993100dd1
github username <github email address>
2018-11-08 12:27:12 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  ans = []
+    array.each do |element|
+      ans.push(element ** 2) # exponent operator
+      # ans << element ** 2 # '<<' MEANS PUSH
+    end
+
+  #array.collect {|element|element ** 2}
+  ans
 end
+
+# can also replace all the above with one line...
+# array.each { |element| ans << element ** 2 }
+
+## Attempt .collect
